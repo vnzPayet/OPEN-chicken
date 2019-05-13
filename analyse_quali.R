7af2900f78864d2403ee9212e7f5dd3c72f469ea
BenoitLecour <blecour@etu.isara.fr>
2019-05-13 16:22:22 +0200

    erreur ligne 22  corrigée

--- analyse_quali.R
+++ analyse_quali.R
@@ -1,10 +1,7 @@
 #Set working directory 
 setwd("C:/Cours/4A/S8/Numérique/Maquette/OPEN-chicken")
-#library(knitr)
-#library(rmarkdown)
-#library(questionr)
-#library(rlang)
-#library(purrr)
+library(rmarkdown)
+
 
 #import des données 
 data_base<-read.table("C:/Cours/4A/S8/Numérique/Maquette/JEU_2.csv", header = TRUE, sep = ";", dec=",",na.strings="NA")
@@ -19,7 +16,7 @@ for (i in 1:p){
     if (nrow(test_id) != nrow(data_base[i])){
     envoi<-data_base[i]
     write.csv(envoi, file = "envoi.csv")
-    outputFile <- paste("Maquette_colonne_quali_", names(data_base)[i], ".Rmd", sep="")
+    outputFile <- paste("Maquette_colonne_quali_", names(data_base)[i], ".html", sep="")
     render("Maquette_colonne_quali.Rmd", output_format = "html_document", output_file = outputFile, encoding = "UTF-8", output_dir = "res")
     #knit("Maquette_colonne_quali.Rmd", output = outputFile, encoding = "UTF-8" )
     #render("Maquette_colonne_quali.Rmd",outputFile, encoding = "UTF-8")
