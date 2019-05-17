afd4831afdbbf29a4a46fcfcd9507fef2d3fbc2c
Nathanjn <njean@etu.isara.fr>
2019-05-17 11:43:46 +0200

    Finition menu

--- app-shiny-principale/APP/app.R
+++ app-shiny-principale/APP/app.R
@@ -43,21 +43,27 @@ ui <- dashboardPage(
             tabItem(tabName = "bienvenue",
                     h2("Bienvenue dans notre application Shiny !"),
                     fluidRow(
-                        box(title = "Utilisation", solidheader = TRUE, collapsible = TRUE,
-                        "Avec cette application web, vous pourrez interpréter vos données :
-                            en colonne, en ligne et générer des rapports à partir de modèles prédéfinis !"),
+                        box(title = "Utilisation", solidheader = TRUE, collapsible = TRUE, background = "navy",
+                            "Avec cette application web, vous pourrez interpréter vos données :
+                            ",br(),"  - En colonne",br(),"  - En ligne ",br(),"  - Et générer des rapports à partir de modèles prédéfinis"),
                         
-                        box(title = "GitHub OPEN-chicken", solidheader = TRUE, collapsible = TRUE,
-                        "Le lien vers le git du projet : https://github.com/vnzPayet/OPEN-chicken/tree/master/app-shiny-principale .
-                        Vous y trouverez tous nos codes, scripts et autres jeu de données, 
+                        box(title = "GitHub OPEN-chicken", solidheader = TRUE, collapsible = TRUE, background = "teal",
+                            "Le lien vers le git du projet : https://github.com/vnzPayet/OPEN-chicken/tree/master/app-shiny-principale ."
+                            ,br(),br(),"Vous y trouverez tous nos codes, scripts et autres jeux de données, 
                         qui nous ont été utiles pour créer cette application web.
-                        Vous pouvez vous en inspirer pour ajuster cette application à vos besoins !"),
+                        ",br(),"Vous pouvez vous en inspirer pour ajuster cette application à vos besoins !"),
                         
-                        box(title = "Être à l'aise avec Shinydashboard", solidheader = TRUE, collapsible = TRUE,
-                            "Shinydashboard est le package qui a permis de réaliser cette interface plutôt soignée.
-                            Vous pouvez prendre en main le package en suivant les conseils et les exemples du lien suivant : 
-                            https://rstudio.github.io/shinydashboard/index.html .") 
-                        
+                        box(title = "Être à l'aise avec Shinydashboard", solidheader = TRUE, collapsible = TRUE, background = "teal",
+                            "Shinydashboard est le package qui a permis de réaliser cette interface plutôt soignée."
+                            ,br(),br(),"Vous pouvez prendre en main le package en suivant les conseils et les exemples du lien suivant : 
+                            ",br(),br(),"rstudio.github.io/shinydashboard/index.html"),
+                        box(title = "Auteurs Version 1", solidheader = TRUE, collapsible = TRUE, background = "navy", "ARCHAMBAUX Juliette", 
+                            br(),"CHENG Jean Rémi",
+                            br(),"LAEBOUSSE Adam",
+                            br(),"LECOUR Benoit",
+                            br(),"JEAN Nathan",
+                            br(),"NAQUIN Samuel",
+                            br(),"RIBOUD Mathilde")
                     )),
             
             # QUALI ----
